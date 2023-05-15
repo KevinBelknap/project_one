5aa549573cf8c9c4172c1e9f03ea97f0cb27bdbe
Yuga <Trev@brennberry.com>
2023-05-15 18:42:45 -0400

    Fixed Weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 const apiKey = '8cf59100d539bf9363c2f7f6beb01c19';
-const newsApiKey ='8aaac5d710ff7de69478231a29c8c791'
+const newsApiKey ='5e2366fdaaed4cd8a56f4864df96e366'
 const newsApiUrl = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8aaac5d710ff7de69478231a29c8c791"
-const apiUrl = `http://api.mediastack.com/v1/news?access_key=8aaac5d710ff7de69478231a29c8c791$&languages=en`;
 
-const WapiKey = '3281a39a9dd34bdcb0704239230905';
+const WapiKey = 'e6e9cf82d2a043fa9be232847230905';
 const locationEl = document.querySelector('.location');
 const temperatureEl = document.querySelector('.temperature');
 const conditionEl = document.querySelector('.condition');
@@ -25,7 +24,14 @@ function updateWeatherInfo(data) {
     locationEl.textContent = `Location: ${data.location.name}, ${data.location.country}`;
     temperatureEl.textContent = `Temperature: ${data.current.temp_f}°F`;
     conditionEl.textContent = `Condition: ${data.current.condition.text}`;
-  }
+
+    // Create an img element and set its source to the weather icon URL
+    const weatherIcon = document.createElement('img');
+    weatherIcon.src = `https:${data.current.condition.icon}`;
+
+    // Append the icon to the condition element
+    conditionEl.appendChild(weatherIcon);
+}
 
 // Set default location
 getWeatherData('New York');
@@ -48,3 +54,4 @@ searchInput.addEventListener('keypress', (e) => {
             searchInput.value = '';
         }
     }
+});
