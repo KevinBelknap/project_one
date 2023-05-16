ac3cad6146d334e73913953c97d4a3492a8c3fcf
jamson00 <jameson.edward.daley@gmail.com>
2023-05-16 18:48:30 -0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const apiKey = '8cf59100d539bf9363c2f7f6beb01c19';
 const gNewsApiKey = 'b31a28696095a791c50b6b1eced1680e';
-const gNewsApiUrl = `https://gnews.io/api/v4/top-headlines?token=${gNewsApiKey}&lang=en`;
+//const gNewsApiUrl = `https://gnews.io/api/v4/top-headlines?token=${gNewsApiKey}&lang=en`;
 
 const WapiKey = 'e6e9cf82d2a043fa9be232847230905';
 const locationEl = document.querySelector('.location');
@@ -25,18 +25,14 @@ function updateWeatherInfo(data) {
     temperatureEl.textContent = `Temperature: ${data.current.temp_f}°F`;
     conditionEl.textContent = `Condition: ${data.current.condition.text}`;
 
-    // Create an img element and set its source to the weather icon URL
     const weatherIcon = document.createElement('img');
     weatherIcon.src = `https:${data.current.condition.icon}`;
 
-    // Append the icon to the condition element
     conditionEl.appendChild(weatherIcon);
   }
 
-// Set default location
 getWeatherData('New York');
 
-// Handle search button click
 searchButton.addEventListener('click', () => {
     const location = searchInput.value;
     if (location) {
@@ -45,7 +41,6 @@ searchButton.addEventListener('click', () => {
     }
 });
 
-// Handle enter key press in search input
 searchInput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
         const location = searchInput.value;
@@ -68,19 +63,15 @@ function getNewsData() {
         });
 }
 function updateNewsInfo(articles) {
-    // Check if there are at least 3 articles
     if (articles.length < 3) {
         console.error('Not enough articles');
         return;
     }
-    // Populate main article
     populateArticle(articles[0], 'main-article');
-    // Populate secondary articles
     populateArticle(articles[1], 'secondary-articles');
     populateArticle(articles[2], 'secondary-articles');
 }
 function populateArticle(article, elementId) {
-    // Create article content
     const title = document.createElement('h3');
     title.textContent = article.title;
     const description = document.createElement('p');
@@ -90,16 +81,13 @@ function populateArticle(article, elementId) {
     link.textContent = 'Read more...';
     const image = document.createElement('img');
     image.src = article.image;
-    // Clear existing content
     const element = document.getElementById(elementId);
     element.innerHTML = '';
-    // Append new content
     element.appendChild(image);
     element.appendChild(title);
     element.appendChild(description);
     element.appendChild(link);
 }
-// Fetch the news data when the page loads
 getNewsData();
 
 function updateBreakingNews(articles) {
